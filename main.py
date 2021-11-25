09e2ee052e1807e80e0444df05ba76391c276ecd
RazdolbayOne <Yone312@gmail.com>
2021-11-25 13:20:09 +0200

    feat: made a grid to table

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from pygame.locals import *
 pygame.init()
 
 # Colours
-BACKGROUND = (255, 255, 255)
+BACKGROUND = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
 
 # Game Setup
 FPS = 60
@@ -13,7 +14,20 @@ WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('My Game!')
+pygame.display.set_caption('Tic Tac Toe')
+
+
+def draw_lines():
+    """Draws grid 3x3 by lines"""
+    # 1 horizontal
+    pygame.draw.line(WINDOW, LINE_COLOR, (0, 200), (WINDOW_WIDTH, 200), 10)
+    # 2 horizontal
+    pygame.draw.line(WINDOW, LINE_COLOR, (0, 400), (WINDOW_WIDTH, 400), 10)
+    # 1 vertical
+    pygame.draw.line(WINDOW, LINE_COLOR, (200, 0), (200, WINDOW_HEIGHT), 10)
+    # 2 vertical
+    pygame.draw.line(WINDOW, LINE_COLOR, (400, 0), (400, WINDOW_HEIGHT), 10)
+
 
 
 # The main function that controls the game
@@ -33,6 +47,7 @@ def main():
 
         # Render elements of the game
         WINDOW.fill(BACKGROUND)
+        draw_lines()
         pygame.display.update()
         fpsClock.tick(FPS)
 
